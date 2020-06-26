bf78d04a49ea564b74c841ea2be904cb154cbba0
fardim <fardim.kaiser@selise.ch>
2020-06-26 19:02:18 +0600

    Section 16 complete

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -50,8 +50,7 @@ namespace API.Controllers
         [HttpGet("emailexists")]
         public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery]string email)
         {
-            var exist = await _userManager.FindByEmailAsync(email) != null;
-            return Ok(exist);
+            return await _userManager.FindByEmailAsync(email) != null;
         }
 
         [Authorize]
@@ -102,6 +101,10 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] { "Email Address is in use" } });
+            }
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
